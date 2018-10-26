a49fcd90581d550e2d207996097d49a578603fd5
Raul Martin Matilla <raul.martin@clarcat.com>
2018-10-26 12:10:49 +0200

    version ultima raul sin IOS

--- PlayMedia/PlayMedia/PlayMedia/App.xaml.cs
+++ PlayMedia/PlayMedia/PlayMedia/App.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 namespace PlayMedia
@@ -17,6 +20,7 @@ namespace PlayMedia
         protected override void OnStart()
         {
             // Handle when your app starts
+            AppCenter.Start("android=3ec6fae8-6df9-48f3-a69c-70da3c5d5bf0;" + "uwp={Your UWP App secret here};" + "ios={Your iOS App secret here}", typeof(Analytics), typeof(Crashes));
         }
 
         protected override void OnSleep()
